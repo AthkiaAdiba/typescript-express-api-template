b0ee38a265bc558a86eac7114caed724a81458a4
Athkia Adiba Tonne <athkiaadiba@gmail.com>
2026-08-08 13:18:16 +0300

    feat: add modular routing and environment configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express"
+import router from "./app/routes"
 
 const app = express()
 
+// Middlewares
 app.use(express.json())
+app.use("/api", router)
+
 
 app.get("/", (_req, res) => {
     res.send("GenAI Study Buddy server is running!")
---
+++ src/app/routes/index.ts
@@ -0,0 +1,15 @@
+import {Router} from "express"
+import { StudyRoutes } from "../modules/study/study.route"
+
+const router = Router()
+
+const moduleRoutes = [
+    {
+        path: "/study",
+        routes: StudyRoutes
+    }
+]
+
+moduleRoutes.forEach((route) => router.use(route.path, route.routes))
+
+export default router
